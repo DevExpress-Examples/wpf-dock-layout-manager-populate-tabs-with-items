c417839e3444fd0e891752deb73ad2578a4269de
DevExpressExampleBot <fakeEmail>
2022-07-08 10:20:34 +0400

    Source auto update [skip ci]

--- VB/MainWindow.xaml.vb
+++ VB/MainWindow.xaml.vb
@@ -1,12 +1,12 @@
-﻿Imports System
 Imports DevExpress.Xpf.Core
 
 Namespace DXSample
-    Partial Public Class MainWindow
+
+    Public Partial Class MainWindow
         Inherits DXWindow
 
         Public Sub New()
-            InitializeComponent()
+            Me.InitializeComponent()
         End Sub
     End Class
 End Namespace
